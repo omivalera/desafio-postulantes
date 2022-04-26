2212638eb9e098f7353f9144a5d05936f3682d73
Richard Espinoza <omivalera@gmail.com>
2022-04-25 23:26:15 -0400

    convertir y ordenar

--- scrapp.py
+++ scrapp.py
@@ -9,3 +9,16 @@ url = 'https://www.sii.cl/servicios_online/1047-nomina_inst_financieras-1714'
 ###En este caso se tomaron los datos del archivo CSV.
 df = pd.DataFrame(pd.read_csv(url + '.csv', sep=';', nrows=None, header=0, index_col=False, encoding='utf-8'))
 df.shape
+
+
+#Convertimos y ordenamos los datos CSV en JSON
+data_json = df.to_json(
+    'scrappdata.json',
+    orient='records', 
+    double_precision = 10, 
+    force_ascii = False, 
+    date_unit = "ms", 
+    default_handler = 
+    None, 
+    indent=4
+    )
