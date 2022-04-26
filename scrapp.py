d24cb58430415e435fe7f4ca10225f471a84851c
Richard Espinoza <omivalera@gmail.com>
2022-04-25 23:23:27 -0400

    first

--- scrapp.py
+++ scrapp.py
@@ -4,3 +4,8 @@ import json
 #Se obtiene la url del sitio
 url = 'https://www.sii.cl/servicios_online/1047-nomina_inst_financieras-1714'
 
+#Se obtiene los datos de la tabla CSV y se guardan en una variable 'df'
+##La tabla es genera mediante Javascript el cual obtiene la informacion de un archivo CSV y lo plasma en la tabla
+###En este caso se tomaron los datos del archivo CSV.
+df = pd.DataFrame(pd.read_csv(url + '.csv', sep=';', nrows=None, header=0, index_col=False, encoding='utf-8'))
+df.shape
